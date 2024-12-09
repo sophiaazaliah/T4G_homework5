ef8d85faf346fea5d2a79688775814f2039cd2d8
Sophiaazaliah <Sophiaazaliah10@gmail.com>
2024-12-09 09:12:25 +0000

    changes

---
+++ src/index.js
@@ -0,0 +1,4 @@
+let friend = prompt ("what is your name?");
+let age = prompt ("How old are you?");
+let email = prompt("what is your emai?");
+alert(`hi your name is ${friend},you are ${age} years old and we will keep in touch via your email:${email}`)
